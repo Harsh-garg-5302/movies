e4cc1706653782db530c1ed44f6308f4bd58ccd4
Harsh-garg-5302 <harshgarg5302@gmail.com>
2022-08-02 12:53:56 +0530

    updated

--- dashboard/views/accounts_dashboard.py
+++ dashboard/views/accounts_dashboard.py
@@ -5,5 +5,7 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST.get("username") or None
         password = request.POST.get("password") or None
+        print(username)
+        print(password)
 
     return render(request, "accounts/login.html")
